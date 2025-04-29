86b9ece1180fd2be6866aea811be160000d45627
Prayag Kasture <prayagkasture@Prayags-MacBook-Air.local>
2025-04-29 12:15:25 +0530

    Test to check for single number

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -4,5 +4,11 @@ require_relative 'string_calculator'
 class StringCalculatorTest < Minitest::Test
     def test_empty_string_returns_zero
         assert_equal 0, StringCalculator.add("")
-      end
+    end
+
+    def test_single_number
+        assert_equal 1, StringCalculator.add("1")
+    end
+
+
 end
